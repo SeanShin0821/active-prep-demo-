cee31694d48348dffa989b2c001e1e641470f146
SeanShin0821 <cshin4@gmu.edu>
2022-01-15 15:42:02 -0500

    java update

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+window.onload = () => {
+    let button = document.querySelector("#btn");
+
+    button.addEventListener("click, calculateBMI")
+}
+
+function calculateBMI() {
+
+    let height = parseInt(document. querySelector("#height").value);
+
+    let weight = parseInt(document. querySelector("#weight").value);
+
+    if (height === "" || isNAN (height))
+        result.innerHTML = "provide a valid Height!";
+    
+    else if (weight === "" || isNAN (weight))
+        result.innerHTML = "provide a valid Weight!";
+
+    else {
+        let bmi = (weight/((height*height)) /10000).toFixed(2);
+
+        if(bmi<18.6) result.innerHTML = 'Under Weight : <span>${bmi}</span>';
+
+        else if (bmi >= 18.6 && bmi<24.9)
+            result.innerHTML = 'Normal : <span>${bmi}</span>'; 
+        
+        else result.innerHTML = 'Over Weight : <span>${bmi}</span>';
+    }
+
+
+}
